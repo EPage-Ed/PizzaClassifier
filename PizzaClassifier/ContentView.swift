41071eef838121895a2f6ccb0c94247732ea03e1
Ed Arenberg <arenberg@epage.com>
2025-03-09 16:07:45 -0700

    Initial Commit

---
+++ PizzaClassifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PizzaClassifier
+//
+//  Created by Edward Arenberg on 3/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PizzaClassifier/PizzaClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaClassifierApp.swift
+//  PizzaClassifier
+//
+//  Created by Edward Arenberg on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PizzaClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
